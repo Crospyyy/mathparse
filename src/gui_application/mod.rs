faef2762a24a0445284992706f7966a624993fe7
Crospyyy <81693484+Crospyyy@users.noreply.github.com>
2025-07-25 00:32:32 +0200

    add functionality to press enter to add new symbol

--- src/gui_application/mod.rs
+++ src/gui_application/mod.rs
@@ -93,9 +93,10 @@ mod controller {
                     self.update_calculation_result();
                 }
 
-                // if response.has_focus() && ui.input(|i| i.key_pressed(egui::Key::Enter)) {
-                //     self.try_apply_calculation();
-                // }
+                if response.lost_focus() && ui.input(|i| i.key_pressed(egui::Key::Enter)) {
+                    self.try_apply_calculation();
+                    response.request_focus();
+                }
                 ui.label(RichText::new(&self.ui_state.calculation_result).size(14.0));
                 let area = ScrollArea::vertical().auto_shrink(false);
                 area.show(ui, |ui| {
