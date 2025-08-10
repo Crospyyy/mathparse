ab635d035dc22c8d2cd8c1e0084587524641fb74
Crospy <81693484+Crospyyy@users.noreply.github.com>
2025-08-10 14:01:12 +0200

    add proc macro

---
+++ macros/src/lib.rs
@@ -0,0 +1,45 @@
+use proc_macro::TokenStream;
+use std::str::FromStr;
+use proc_macro2::{TokenStream as TokenStream2, TokenTree};
+use quote::quote;
+
+#[proc_macro]
+pub fn match_formula_proc(item: TokenStream) -> TokenStream {
+	let input: TokenStream2 = item.into();
+	let mut iter = input.into_iter();
+	// expr bis Komma
+	let mut expr_tokens = Vec::new();
+	while let Some(tt) = iter.next() {
+		if let TokenTree::Punct(p) = &tt {
+			if p.as_char() == ',' {
+				break;
+			}
+		}
+		expr_tokens.push(tt);
+	}
+	let expr_ts: TokenStream2 = expr_tokens.into_iter().collect();
+	// ident
+	let ident = match iter.next() {
+		Some(TokenTree::Ident(i)) => i,
+		_ => panic!("expected identifier"),
+	};
+	// optionale innere Tokentrees
+	let inner_group = iter.next().map(|tt| match tt {
+		TokenTree::Group(g) => g.stream(),
+		_ => panic!("unexpected token after identifier"),
+	});
+	if iter.next().is_some() {
+		panic!("unexpected tokens after inner group");
+	}
+	let name = ident.to_string();
+	let inner_group = inner_group.unwrap_or(TokenStream2::from_str("()").unwrap());
+	let expanded = quote! {
+        {
+            let __expr = #expr_ts;
+            let __name = #name;
+            let __inner = #inner_group;
+            (__expr, __name, __inner)
+        }
+    };
+	expanded.into()
+}
